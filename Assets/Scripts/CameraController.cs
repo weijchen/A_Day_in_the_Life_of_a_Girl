8ad2b25a4b69b50644c76df03cfd2258fc64cefb
weijchen0919 <weijchen0919@gmail.com>
2021-10-19 23:01:09 -0400

    add zoom event to click script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera _camera;
+    private float startZoomIn;
+    private float startX;
+    private float startY;
+    private float startZ;
+
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+        startZoomIn = _camera.orthographicSize;
+        startX = gameObject.transform.position.x;
+        startY = gameObject.transform.position.y;
+        startZ = gameObject.transform.position.z;
+    }
+
+    public void ZoomIn(ZoomProperties zoomProperties)
+    {
+        StartCoroutine(StartZoomIn(zoomProperties));
+    }
+    
+    IEnumerator StartZoomIn(ZoomProperties zoomProperties)
+    {
+        float progress = 0;
+        float inc = zoomProperties.smoothness / zoomProperties.zoomInTime;
+
+        while (progress < zoomProperties.zoomInStay/zoomProperties.zoomInTime)
+        {
+            _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, zoomProperties.zoomInSize, progress);
+            _camera.transform.position = Vector3.Lerp(_camera.transform.position, zoomProperties.zoomInPosition.position, progress);
+            progress += inc;
+            if (progress >= zoomProperties.zoomInStay/zoomProperties.zoomInTime)
+            {
+                ZoomOut(zoomProperties.smoothness, zoomProperties.zoomOutTime);
+            }    
+            yield return new WaitForSeconds(zoomProperties.smoothness);
+        }
+    }
+
+    public void ZoomOut(float smoothness, float zoomOutTime)
+    {
+        StartCoroutine(StartZoomOut(smoothness, zoomOutTime));
+    }
+
+    IEnumerator StartZoomOut(float smoothness, float duration)
+    {
+        float progress = 0;
+        float inc = smoothness / duration;
+
+        while (progress < duration)
+        {
+            _camera.orthographicSize = Mathf.Lerp(_camera.orthographicSize, startZoomIn, progress);
+            _camera.transform.position = Vector3.Lerp(_camera.transform.position, new Vector3(startX, startY, startZ), progress);
+            progress += inc;
+            
+            yield return new WaitForSeconds(smoothness);
+        }
+    }
+}
--- Assets/Scripts/Click.cs
+++ Assets/Scripts/Click.cs
@@ -8,11 +8,38 @@ using UnityEngine.Events;
 public class ClickEvent : UnityEvent<string>
 {
 
-} 
+}
+
+[System.Serializable]
+public class ZoomEvent : UnityEvent<ZoomProperties>
+{
+
+}
+
+public struct ZoomProperties
+{
+    public Transform zoomInPosition;
+    public float zoomInSize;
+    public float smoothness;
+    public float zoomInTime;
+    public float zoomInStay;
+    public float zoomOutTime;
+}
+
 public class Click : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Zoom In")]
+    [SerializeField] Transform zoomInPosition;
+    [SerializeField][Range(1.0f, 9.8f)] private float zoomInSize;
+    [SerializeField] private float smoothness = 0.01f;
+    [SerializeField] private float zoomInTime = 1.0f;
+    [SerializeField] private float zoomInStay = 2.0f;
+    [SerializeField] private float zoomOutTime = 1.0f;
+    
     public ClickEvent clickEvent;
+    public ZoomEvent zoomEvent;
+    public ZoomProperties zoomProperties;
+    
     void Start()
     {
         if (clickEvent == null)
@@ -21,7 +48,6 @@ public class Click : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -30,6 +56,14 @@ public class Click : MonoBehaviour
             if (hit.collider != null &&hit.collider.gameObject==this.gameObject)
             {
                 clickEvent.Invoke("11");
+                zoomProperties.zoomInPosition = zoomInPosition;
+                zoomProperties.zoomInSize = zoomInSize;
+                zoomProperties.smoothness = smoothness;
+                zoomProperties.zoomInTime = zoomInTime;
+                zoomProperties.zoomInStay = zoomInStay;
+                zoomProperties.zoomOutTime = zoomOutTime;
+                zoomEvent.Invoke(zoomProperties);
+                
                 //Debug.Log(hit.collider.gameObject.name);
             }
 
